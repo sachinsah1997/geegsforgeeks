100a0c307085425709395b6ad7af7efa34b53b8b
Sachin Sah <39879597+sachinsah1997@users.noreply.github.com>
2021-03-06 21:57:38 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            String ans = ob.is_palindrome(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String is_palindrome(int n){
+        
+        String numStr = Integer.toString(n);
+        int slength  = numStr.length();
+        int i=0, j=slength-1;
+        while(i<j){
+            if(numStr.charAt(i) == numStr.charAt(j)){
+                i++;
+                j--;
+            }else{
+                return "No";
+            }            
+        }
+        return "Yes";
+    
+    }
+}
