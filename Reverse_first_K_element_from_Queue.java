2d34e6f72fb2cd99a304cb1a2ca90ebc4bc7f60c
Sachin Sah <39879597+sachinsah1997@users.noreply.github.com>
2021-03-06 22:12:23 +0530

    Create Reverse_first_K_element_from_Queue.java

---
+++ Reverse_first_K_element_from_Queue.java
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class ModifyQueue{
+    public static void main(String[] args){
+        
+        //Taking input using class Scanner
+        Scanner sc=new Scanner(System.in);
+        
+        //Taking total number of testcases
+        int t=sc.nextInt();
+        
+        while(t-->0){
+            //Taking count of total number of elements
+            int n=sc.nextInt();
+            
+            //Taking count of total elements
+            //that need to be reversed
+            int k=sc.nextInt();
+            
+            //Creating a Queue
+            Queue<Integer> q=new LinkedList<>();
+            
+            //adding all the elements to the Queue
+            while(n-->0){
+                q.add((int)sc.nextInt());
+            }
+            
+            //Creating an object of class GfG
+            GfG g=new GfG();
+            
+            //calling modifyQueue of class GfG
+            //and passing Queue and k as arguments
+            //and storing the reuslt in a new Queue
+            Queue<Integer> ans=g.modifyQueue(q,k);
+            
+            //Printing all the elements from the
+            //new Queue and polling them out
+            while(!ans.isEmpty()){
+                int a=ans.peek();
+                ans.poll();
+                System.out.print(a+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class GfG
+{
+    public Queue<Integer> modifyQueue(Queue<Integer> q, int k){
+      
+      int size = q.size();
+      Stack<Integer> stack = new Stack<>();
+      for(int i=0; i<k; i++){
+          stack.push(q.poll());
+      }
+      
+      Queue<Integer> qn = new LinkedList<>();
+      for(int i=0; i<k; i++){
+          qn.offer(stack.pop());
+      }
+      
+      for(int i=k; i<size; i++){
+          qn.offer(q.poll());aaa
+      }
+      return qn;a
+    }
+}
