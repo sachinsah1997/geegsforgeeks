e7d74121b3fc61091b6095bb17a2bd20a106c42a
Sachin Sah <39879597+sachinsah1997@users.noreply.github.com>
2021-03-07 14:01:41 +0530

    Create Find_the_highet_number.java

---
+++ Find_the_highet_number.java
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine().trim();
+            int n = Integer.parseInt(s);
+            String S = br.readLine();
+            String[] s1 = S.split(" ");
+            List<Integer> a = new ArrayList<Integer>(); 
+            for( int i = 0; i < n; i++)
+            {
+                a.add(Integer.parseInt(s1[i]));
+            }
+            Solution ob = new Solution();
+            int ans = ob.findPeakElement(a);
+            System.out.println(ans);
+
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int findPeakElement(List<Integer> a){
+        
+        int low=0, high=a.size()-1;
+        int mid = 0;
+        
+        while(low<high){
+            
+            mid = (low+high)/2;
+           // System.out.println(mid);
+            
+            if(a.get(mid) > a.get(mid+1) && a.get(mid) > a.get(mid-1)){
+                return a.get(mid);
+            }else if(a.get(mid) > a.get(mid+1)){
+                high = mid-1;
+            }else {
+                low = mid+1;
+            }
+        }
+        return a.get(high);
+        //return Collections.max(a);
+    }
+}
