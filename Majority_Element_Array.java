79bd545cbd8ca0c34f5249a3adf152a83f080f0b
Sachin Sah <39879597+sachinsah1997@users.noreply.github.com>
2021-05-01 11:25:21 +0530

    Find Majority ELement from the array with extra space

---
+++ Majority_Element_Array.java
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().majorityElement(arr, n)); 
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int majorityElement(int a[], int size)
+    {
+        int count=1;
+        int majElement = a[0];
+        
+        for(int i=1; i<size; i++){
+            
+            if(majElement == a[i]){
+                    count++;
+            }else{
+                if(count==1){
+                    majElement = a[i];
+                }else{
+                    count--;
+                }
+            }             
+        }
+        
+        count=0;
+        for(int i=0; i<size; i++){
+            if(a[i] == majElement){
+                count++;
+            }
+        }
+        
+        if(count > size/2){
+            return majElement;
+        }
+        
+        return -1;
+    }
+}
