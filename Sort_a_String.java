68e8cec78ed127477adeeafc4b2f5720b84ea879
Sachin Sah <39879597+sachinsah1997@users.noreply.github.com>
2021-05-13 09:06:03 +0530

    Sort a String

---
+++ Sort_a_String.java
@@ -0,0 +1,31 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine().trim());
+        while(t-- > 0){
+            String s = sc.nextLine().trim();
+            Solution obj = new Solution();
+            System.out.println(obj.sort(s));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    String sort(String s) 
+    {
+        char[] charArr = s.toCharArray();
+        
+        Arrays.sort(charArr);
+        
+        String revStr = new String(charArr);
+        return revStr;
+    }
+} 
