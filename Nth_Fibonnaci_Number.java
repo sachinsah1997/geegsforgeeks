d5bbd4b6d0783c84e7538bdf7dff37e1f7c0cdae
Sachin Sah <39879597+sachinsah1997@users.noreply.github.com>
2021-05-18 21:39:36 +0530

    Nth Fibonacci Number
    Given a positive integer n, find the nth fibonacci number. Since the answer can be very large, return the answer modulo 1000000007.

---
+++ Nth_Fibonnaci_Number.java
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            long n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.nthFibonacci(n));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+//User function Template for Java
+class Solution {
+    static long nthFibonacci(long n){
+        
+        long ans = fib((int)n);
+        return ans;
+    }
+    
+    static long fib(int n){
+        
+        long dp[] = new long[n+1];
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            dp[i] = (dp[i-1]+dp[i-2])%1000000007;
+        }
+        
+        return dp[n];
+    } 
+}
