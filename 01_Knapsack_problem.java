dd8a8d38946c5b1e32e6b5db17134f06062c5988
Sachin Sah <39879597+sachinsah1997@users.noreply.github.com>
2021-05-19 18:35:41 +0530

    0 - 1 Knapsack Problem
    You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.
    In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights associated with N items respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that sum of the weights of this subset is smaller than or equal to W. You cannot break an item, either pick the complete item, or don’t pick it (0-1 property)

--- 01_Knapsack_problem.java
+++ 01_Knapsack_problem.java
@@ -35,7 +35,7 @@ class gfg
               wt[i] = Integer.parseInt(s[i]);
               
             //calling method knapSack() of class Knapsack
-            System.out.println(new Knapsack().knapSack(w, wt, val, n));
+            System.out.println(new Solution().knapSack(w, wt, val, n));
         }
     }
 }
@@ -45,27 +45,37 @@ class gfg
 // } Driver Code Ends
 
 
-class Knapsack 
+
+
+
+class Solution 
 { 
-    // Returns the maximum value that can be put in a knapsack of capacity W 
+    //Function to return max value that can be put in knapsack of capacity W.
     static int knapSack(int W, int wt[], int val[], int n) 
     { 
+         
          int dp[][] = new int[n+1][W+1];
          
-         for(int i=0; i<=n; i++){
-             for(int j=0; j<=W; j++){
-                 
-                 if(i==0 || j==0){
-                     dp[i][j] = 0;
-                 }else if(wt[i-1]<=j){
-                    dp[i][j] = Math.max(dp[i-1][j], val[i-1] + dp[i-1][j-wt[i-1]]); 
-                 }else{
-                     dp[i][j] = dp[i-1][j];
-                 } 
-                     
-            }
+         for(int i=1; i<=n; i++){
+             for(int j=1; j<=W; j++){
+                 dp[i][j] = -1;
+             }
          }
-         return dp[n][W];
          
+         for(int i=1; i<=n; i++){
+             for(int j=1; j<=W; j++){
+                
+                if(wt[i-1]<=j){
+                    dp[i][j] = Math.max((val[i-1] + dp[i-1][j-wt[i-1]]), dp[i-1][j]); 
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+                
+             }
+         }
+         
+         return dp[n][W];
     } 
 }
+
+
