1d56a42ed0d18b1f9f0da8bfcfc9a509aab64442
Sachin Sah <39879597+sachinsah1997@users.noreply.github.com>
2021-05-26 21:00:37 +0530

    Merge Without Extra Space
    Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order without using any extra space. Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.

--- merge_without_extra_spcae.java
+++ merge_without_extra_spcae.java
@@ -1,60 +1,62 @@
 // { Driver Code Starts
-import java.io.*;
+//Initial Template for Java
+
 import java.util.*;
+import java.io.*;
+import java.io.*;
 
-class GFG {
-    // Driver code
-    public static void main(String[] args) throws Exception {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            String inputLine[] = br.readLine().trim().split(" ");
-            int n = Integer.parseInt(inputLine[0]);
-            int m = Integer.parseInt(inputLine[1]);
-            int arr1[] = new int[n];
-            int arr2[] = new int[m];
-            inputLine = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++) {
-                arr1[i] = Integer.parseInt(inputLine[i]);
+public class Main
+{
+    public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int m = Integer.parseInt(inputLine[1]);
+		    long arr1[] = new long[n];
+		    long arr2[] = new long[m];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        arr1[i] = Long.parseLong(inputLine[i]);
+		    }
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<m; i++){
+		        arr2[i] = Long.parseLong(inputLine[i]);
+		    }
+		    Solution ob = new Solution();
+		    ob.merge(arr1, arr2, n, m);
+		    
+		    StringBuffer str = new StringBuffer();
+		    for(int i=0; i<n; i++){
+		        str.append(arr1[i]+" ");
+		    }
+		    for(int i=0; i<m; i++){
+		        str.append(arr2[i]+" ");
+		    }
+		    System.out.println(str);
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to merge the arrays.
+    public static void merge(long arr1[], long arr2[], int n, int m) 
+    {
+        int i=n-1,j=0;
+        while(i>=0 && j<m){
+            if(arr1[i] >= arr2[j]){
+                long temp = arr1[i];
+                arr1[i] = arr2[j];
+                arr2[j] = temp;
+                j++;
             }
-            inputLine = br.readLine().trim().split(" ");
-            for (int i = 0; i < m; i++) {
-                arr2[i] = Integer.parseInt(inputLine[i]);
-            }
-
-            new Solution().merge(arr1, arr2, n, m);
-
-            StringBuffer str = new StringBuffer();
-            for (int i = 0; i < n; i++) {
-                str.append(arr1[i] + " ");
-            }
-            for (int i = 0; i < m; i++) {
-                str.append(arr2[i] + " ");
-            }
-            System.out.println(str);
-        }
-    }
-}// } Driver Code Ends
-
-
-class Solution {
-
-    public void merge(int arr1[], int arr2[], int n, int m) {
-        
-        int x = n-1, y=0;
-        
-        while(x>=0 && y<m){
-            
-            if(arr1[x] >= arr2[y]){
-                int tmp = arr1[x];
-                arr1[x] = arr2[y];
-                arr2[y] = tmp;
-                x--;
-                y++;
-            }else{
-                x--;
-            }
+                i--;
         }
         
         Arrays.sort(arr1);
