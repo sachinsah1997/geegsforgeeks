4cdd23a41da7b93612fb3f7fd7e954a280d33817
Sachin Sah <39879597+sachinsah1997@users.noreply.github.com>
2021-05-27 20:23:35 +0530

    Update Maximum_Index.java

--- Maximum_Index.java
+++ Maximum_Index.java
@@ -1,1 +1,57 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim()); // size of array
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]); // input elements of array
+		    }
+		    
+		    Solution ob = new Solution();
+		    
+		    System.out.println(ob.maxIndexDiff(arr, n)); // print the result
+		}
+	}
+}// } Driver Code Ends
+
+
+
+
+class Solution{
+    
+    // arr[]: input array
+    // n: size of array
+    // Function to find the maximum index difference.
+    static int maxIndexDiff(int arr[], int n) { 
+        
+        //O(N*(n-1)
+        int max = -1;
+        int j=n-1;
+        int i=0;
+        while(i<=j){
+            
+            if(arr[i]<=arr[j]){
+                if(max < j-i){
+                    max = j-i;
+                }
+                i++;
+                j=n-1;
+            }else{
+                j--;
+            }
+            
+        }
+        
+        return max;
+    }
+}
+
 
